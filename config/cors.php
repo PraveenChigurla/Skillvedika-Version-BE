ce55cbae222d07cf3e1e02938d0e6165c2ce310d
Sreekar Reddy <112748777+sreekar-2002@users.noreply.github.com>
2026-01-16 17:23:31 +0530

    -cor

--- config/cors.php
+++ config/cors.php
@@ -12,7 +12,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => array_filter(
-        explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:8000'))
+        explode(',', env('CORS_ALLOWED_ORIGINS', ''))
     ),
 
     'allowed_origins_patterns' => [],
